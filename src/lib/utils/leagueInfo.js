8e46fc272b98873f623481a00182f504709341f4
BravesVolsUU <88356004+BravesVolsUU@users.noreply.github.com>
2023-06-22 00:09:23 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -73,6 +73,7 @@ export const managers = [
        "bio": "My name is Bennett Wilson.",
        "photo": "/managers/bennett.png", // square ratio recommended (no larger than 500x500)
        "favoriteTeam": "ten", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Trey Lance Haters", // Can be anything (usually your rival's name)
          link: null, // manager array number within this array, or null to link back to all managers page
